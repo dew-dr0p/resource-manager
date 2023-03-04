4a91b21456d6bea48b49b9199d4f62b74bf00b48
Wisdom-Daniel Efe <efewisdomd01@gmail.com>
2023-03-04 16:16:39 +0100

    Initialised Git Repository

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import NavBar from './components/NavBar.vue'
+</script>
+
+<template>
+  <header>
+    <nav>
+      <NavBar />
+    </nav>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+/* @tailwind base;
+@tailwind components;
+@tailwind utilities;
+@tailwind variants; */
+</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <RouterLink to="/">Staff Login</RouterLink>
+    <RouterLink to="/">Staff Login</RouterLink>
+    <RouterLink to="/">Staff Login</RouterLink>
+    <p class="bg-red-500">jkhekj</p>
+  </div>
+</template>
