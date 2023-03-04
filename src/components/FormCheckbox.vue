edde09a59c2f713214f3b13bb8965c78e7f70bd7
Wisdom-Daniel Efe <efewisdomd01@gmail.com>
2023-03-04 19:43:26 +0100

    Added forgot password link

--- src/components/FormCheckbox.vue
+++ src/components/FormCheckbox.vue
@@ -1,4 +1,7 @@
 <template>
-    <input type="checkbox" class="mr-2 mb-5">
-    <span>Remember me</span>
+    <div class="inline-grid grid-flow-col items-center mb-4 w-full">
+        <input type="checkbox" class="justify-self-start">
+        <span class="-ml-14">Remember me</span>
+        <span class="text-[#41521F] justify-self-end font-bold"><RouterLink to="/">Forgot Password?</RouterLink></span>
+    </div>
 </template>
--- src/components/FormInput.vue
+++ src/components/FormInput.vue
@@ -7,6 +7,8 @@ const props = defineProps<{
 </script>
 
 <template>
-    <h5 class="mb-2">{{props.title}}</h5>
-    <input :type="props.type" :placeholder="props.text" class="border rounded-xl w-full border-gray-300 p-3 mb-5">
+    <div>
+        <h5 class="mb-2">{{props.title}}</h5>
+        <input :type="props.type" :placeholder="props.text" class="border rounded-xl w-full border-gray-300 p-3 mb-5">
+    </div>
 </template>
